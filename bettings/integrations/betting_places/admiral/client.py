8ff45d561f80b22002340491b66f97ed7b5b307a
ghajduk3 <gh8590@student.uni-lj.si>
2022-01-22 22:17:43 +0100

    Base client added. Extracted common methods

--- bettings/integrations/betting_places/admiral/client.py
+++ bettings/integrations/betting_places/admiral/client.py
@@ -17,24 +17,20 @@ from bettings import enums as betting_enums
 from bettings.integrations.betting_places import enums as bet_place_enums
 from bettings.integrations.betting_places import constants as bet_place_constants
 from bettings.integrations.betting_places import exceptions as betting_exceptions
+from bettings.integrations.betting_places import base as base_integration
+
 
 logger = logging.getLogger(__name__)
 _LOG_PREFIX = "[ADMIRAL_CLIENT]"
 
 
-class AdmiralBaseClient(object):
+class AdmiralBaseClient(base_integration.IntegrationBaseClient):
     def __init__(self, sport, headless=True):
-        # type: (betting_enums.Sports, typing.Dict) -> None
+        # type: (betting_enums.Sports, bool) -> None
+        super(AdmiralBaseClient, self).__init__(headless)
         self.url = settings.CLIENT_SPORT_URLS[
             bet_place_enums.BettingInstitutions.ADMIRAL.name.upper()
         ][sport.name.upper()]
-        self.driver = self._get_driver(headless)
-
-    def _get_driver(self, headless):
-        fireFoxOptions = options.Options()
-        fireFoxOptions.headless = headless
-        return webdriver.Firefox(options=fireFoxOptions, service=Service(GeckoDriverManager().install()))
-
 
 class AdmiralSoccerClient(AdmiralBaseClient):
     def __init__(self):
@@ -238,45 +234,6 @@ class AdmiralSoccerClient(AdmiralBaseClient):
 
 
 
-
-    @staticmethod
-    def _get_element(driver, x_path):
-        try:
-            return driver.find_element(By.XPATH, x_path)
-        except selenium_exceptions.NoSuchElementException as e:
-            raise betting_exceptions.XpathElementNotFoundException(str(e))
-        except Exception as e:
-            raise betting_exceptions.XpathGeneralException(str(e))
-
-    @staticmethod
-    def _get_elements(driver, x_path):
-        try:
-            elements = driver.find_elements(By.XPATH, x_path)
-            if not elements:
-                raise betting_exceptions.XpathElementsNotFoundError()
-            return elements
-        except Exception as e:
-            raise betting_exceptions.XpathGeneralException(str(e))
-
-    @staticmethod
-    def _switch_frame(driver, frame):
-        try:
-            driver.switch_to.frame(frame)
-        except selenium_exceptions.NoSuchFrameException as e:
-            raise betting_exceptions.XpathFrameNotFoundException(str(e))
-        except Exception as e:
-            raise betting_exceptions.XpathGeneralException(str(e))
-
-    @classmethod
-    def _select_element_by_visible_text(cls, driver, x_path, text):
-        try:
-            select_element = select.Select(cls._get_element(driver, x_path))
-            select_element.select_by_visible_text(text=text)
-        except selenium_exceptions.NoSuchElementException as e:
-            raise betting_exceptions.XpathElementNotFoundException(e)
-        except Exception as e:
-            raise betting_exceptions.XpathGeneralException(e)
-
     @classmethod
     def _get_match_odds(cls, driver):
         one = cls._get_element(
@@ -309,21 +266,3 @@ class AdmiralSoccerClient(AdmiralBaseClient):
             bet_place_enums.FootballMatchPlays.XTWO.value: float(extwo),
             bet_place_enums.FootballMatchPlays.ONETWO.value: float(onetwo),
         }
-
-
-    @staticmethod
-    def _get_normalized_soccer_team_info(team_name):
-        text = re.sub(r"[^a-zA-Z0-9\sčČšŠžŽ]", "", team_name)
-        # remove multiple white spaces
-        text = re.sub(' +', ' ', text)
-        # convert all letters to lower case
-        text = text.lower().strip()
-        text = sorted(text.split(' '), key=len)
-        if len(text) >= 2:
-            first, second = text[-2:]
-            if len(first) == len(second):
-                return first + " " + second
-            else:
-                return second
-
-        return text[0]
--- bettings/integrations/betting_places/meridian/client.py
+++ bettings/integrations/betting_places/meridian/client.py
@@ -18,23 +18,20 @@ from bettings import enums as betting_enums
 from bettings.integrations.betting_places import enums as bet_place_enums
 from bettings.integrations.betting_places import constants as bet_place_constants
 from bettings.integrations.betting_places import exceptions as betting_exceptions
+from bettings.integrations.betting_places import base as base_integration
+
 
 logger = logging.getLogger(__name__)
 _LOG_PREFIX = "[MERIDIAN_CLIENT]"
 
 
-class MeridianBaseClient(object):
+class MeridianBaseClient(base_integration.IntegrationBaseClient):
     def __init__(self, sport, headless=True):
-        # type: (betting_enums.Sports, typing.Dict) -> None
+        # type: (betting_enums.Sports, bool) -> None
+        super(MeridianBaseClient, self).__init__(headless)
         self.url = settings.CLIENT_SPORT_URLS[
             bet_place_enums.BettingInstitutions.MERIDIAN.name.upper()
         ][sport.name.upper()]
-        self.driver = self._get_driver(headless)
-
-    def _get_driver(self, headless):
-        fireFoxOptions = options.Options()
-        fireFoxOptions.headless = headless
-        return webdriver.Firefox(options=fireFoxOptions, service=Service(GeckoDriverManager().install()))
 
 
 class MeridianSoccerClient(MeridianBaseClient):
@@ -174,60 +171,6 @@ class MeridianSoccerClient(MeridianBaseClient):
         day, month, _ = date.split(".")
 
         return datetime.datetime(datetime.date.today().year, int(month), int(day), int(hour), int(minutes))
-    @staticmethod
-    def _get_element(driver, x_path):
-        try:
-            return driver.find_element(By.XPATH, x_path)
-        except selenium_exceptions.NoSuchElementException as e:
-            raise betting_exceptions.XpathElementNotFoundException(str(e))
-        except Exception as e:
-            raise betting_exceptions.XpathGeneralException(str(e))
-
-    @staticmethod
-    def _get_elements(driver, x_path):
-        try:
-            elements = driver.find_elements(By.XPATH, x_path)
-            if not elements:
-                raise betting_exceptions.XpathElementsNotFoundError()
-            return elements
-        except Exception as e:
-            raise betting_exceptions.XpathGeneralException(str(e))
-
-    @staticmethod
-    def _switch_frame(driver, frame):
-        try:
-            driver.switch_to.frame(frame)
-        except selenium_exceptions.NoSuchFrameException as e:
-            raise betting_exceptions.XpathFrameNotFoundException(str(e))
-        except Exception as e:
-            raise betting_exceptions.XpathGeneralException(str(e))
-
-    @classmethod
-    def _select_element_by_visible_text(cls, driver, x_path, text):
-        try:
-            select_element = select.Select(cls._get_element(driver, x_path))
-            select_element.select_by_visible_text(text=text)
-        except selenium_exceptions.NoSuchElementException as e:
-            raise betting_exceptions.XpathElementNotFoundException(e)
-        except Exception as e:
-            raise betting_exceptions.XpathGeneralException(e)
-
-    @staticmethod
-    def _get_normalized_soccer_team_info(team_name):
-        text = re.sub(r"[^a-zA-Z0-9\sčČšŠžŽ]", "", team_name)
-        # remove multiple white spaces
-        text = re.sub(' +', ' ', text)
-        # convert all letters to lower case
-        text = text.lower().strip()
-        text = sorted(text.split(' '), key=len)
-        if len(text) >= 2:
-            first, second = text[-2:]
-            if len(first) == len(second):
-                return first + " " + second
-            else:
-                return second
-
-        return text[0]
 
     @staticmethod
     def _parse_odd(odd):
